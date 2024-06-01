73f648d1ecc715aa0fce6f746ea88419cc45f9a9
Nanyria <145432308+Nanyria@users.noreply.github.com>
2024-06-01 15:53:50 +0200

    Add project files.

---
+++ IndProjModels/Appointment.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndUppClassModels
+{
+    public class Appointment
+    {
+
+        public int AppointmentID { get; set; }
+        public DateTime DateTime { get; set; }
+        
+    }
+    public class AppointmentTime
+    {
+        date hour = 
+    }
+    public class AppointmentList : List<Appointment> { }
+}
